b0ab9a305d2c9a8322c515f8b9a2badc62438ca4
harrislious1-cmd <harrislious1@gmail.com>
2026-07-16 04:07:40 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const pLimit = require('p-limit');
 const rateLimit = require('express-rate-limit');
 
 const app = express();
-const app = express();
 app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3000;
 
